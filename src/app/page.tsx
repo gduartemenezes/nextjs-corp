d0432d420530920e20bba2eab7c8e33be9a4c342
“Gustavo <gduartemenezes@gmail.com>
2024-09-30 08:15:04 -0300

    feat: add linking between pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <div>Home page</div>
+    <div>
+      <div>
+        <Link href={"/performance"}>performance</Link>
+        <Link href={"/scale"}>scale</Link>
+        <Link href={"/reliability"}>reliability</Link>
+      </div>
+      <div>Home page</div>
+    </div>
   );
 }
