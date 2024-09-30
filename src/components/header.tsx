7cad3935387c92437d8fe6701c903f4f5986f43e
“Gustavo <gduartemenezes@gmail.com>
2024-09-30 08:21:50 -0300

    chore: format file

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 const Header = () => {
   return (
     <div>
-          <Link href={"/"}>home</Link>
-          <Link href={"/performance"}>performance</Link>
-          <Link href={"/scale"}>scale</Link>
-          <Link href={"/reliability"}>reliability</Link>
-        </div>
-  )
-}
+      <Link href={"/"}>home</Link>
+      <Link href={"/performance"}>performance</Link>
+      <Link href={"/scale"}>scale</Link>
+      <Link href={"/reliability"}>reliability</Link>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
