68ffaa23f72a5319b5d96fe70fbe13bc858c1527
“Gustavo <gduartemenezes@gmail.com>
2024-09-30 08:36:10 -0300

    feat: add image to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+import homeImg from "public/home.jpg";
 export default function Home() {
   return (
     <div>
-     
-      <div>Home page</div>
+      Home page
+      <div className="absolut -z-10 inset-0">
+        <Image
+          alt="car factory"
+          src={homeImg}
+          fill
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </div>
     </div>
   );
 }
